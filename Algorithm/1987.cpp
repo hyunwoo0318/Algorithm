23fd1b962fb3db9a358f92b7339a2093868ad253
Hyunwoo Lim <hyunwoo0318@naver.com>
2022-03-23 23:13:18 +0900

    1987

---
+++ Algorithm/1987.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+char arr[21][21];
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+
+	for (int i = 0; i < a; i++)
+	{
+		string s;
+		cin >> s;
+
+	}
+}
